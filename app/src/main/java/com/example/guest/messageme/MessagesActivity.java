6ba72b09f496d8335720a7ddf80298aed0fa786b
5adiyah <sadiali@pdx.edu>
2016-07-13 10:33:20 -0700

    Add New Message activity

--- app/src/main/java/com/example/guest/messageme/MessagesActivity.java
+++ app/src/main/java/com/example/guest/messageme/MessagesActivity.java
@@ -1,13 +1,30 @@
 package com.example.guest.messageme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MessagesActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class MessagesActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.newMessage) ImageView mNewMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        ButterKnife.bind(this);
+
+        mNewMessage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v==mNewMessage){
+            Intent intent = new Intent(MessagesActivity.this, NewMessageActivity.class);
+            startActivity(intent);
+        }
     }
 }
