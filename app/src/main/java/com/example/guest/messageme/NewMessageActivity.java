bc55d7ff4bb993d52d02b3be8e3fbb9d8ede9ff4
5adiyah <sadiali@pdx.edu>
2016-07-13 11:08:26 -0700

    Add ValueEventListener

--- app/src/main/java/com/example/guest/messageme/NewMessageActivity.java
+++ app/src/main/java/com/example/guest/messageme/NewMessageActivity.java
@@ -3,12 +3,16 @@ package com.example.guest.messageme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,6 +32,21 @@ public class NewMessageActivity extends AppCompatActivity implements View.OnClic
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_MESSAGE);
+
+        mMessageReference.addValueEventListener(new ValueEventListener() {
+            @Override //called whenever data at the specified node changes.
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot messageSnapshot : dataSnapshot.getChildren()){
+                    String message = messageSnapshot.getValue().toString();
+                    Log.d("Message", message);
+                }
+            }
+
+            @Override //called if the listener is unsuccessful for any reason.
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         ButterKnife.bind(this);
         mSendMessage.setOnClickListener(this);
