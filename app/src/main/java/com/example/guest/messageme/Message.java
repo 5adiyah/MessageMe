a7f40336473ed31feaa042112f4aaf3c31cbfe49
5adiyah <sadiali@pdx.edu>
2016-07-13 11:49:01 -0700

    Lunch commit

---
+++ app/src/main/java/com/example/guest/messageme/Message.java
@@ -0,0 +1,18 @@
+package com.example.guest.messageme;
+
+/**
+ * Created by Guest on 7/13/16.
+ */
+public class Message {
+    String message;
+
+    public Message(){}
+
+    public Message(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
