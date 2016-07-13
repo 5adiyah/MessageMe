d9bdd4ea4ace9be99fa34b98ca97df8c2ac464e9
5adiyah <sadiali@pdx.edu>
2016-07-13 10:48:32 -0700

    At this point, it writes the message to the database

---
+++ app/src/main/java/com/example/guest/messageme/Constants.java
@@ -0,0 +1,8 @@
+package com.example.guest.messageme;
+
+/**
+ * Created by Guest on 7/13/16.
+ */
+public class Constants {
+    public static final String FIREBASE_CHILD_MESSAGE = "message";
+}
--- app/src/main/java/com/example/guest/messageme/NewMessageActivity.java
+++ app/src/main/java/com/example/guest/messageme/NewMessageActivity.java
@@ -1,13 +1,54 @@
 package com.example.guest.messageme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class NewMessageActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewMessageActivity extends AppCompatActivity implements View.OnClickListener{
+    private DatabaseReference mMessageReference;
+
+    @Bind(R.id.sendMessage) ImageView mSendMessage;
+    @Bind(R.id.messageText) EditText mMessageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        mMessageReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_MESSAGE);
+
+        ButterKnife.bind(this);
+        mSendMessage.setOnClickListener(this);
+        mMessageText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v==mSendMessage){
+            String message = mMessageText.getText().toString();
+            saveMessageToFirebase(message);
+
+            Intent intent = new Intent(NewMessageActivity.this, MessagesActivity.class);
+            intent.putExtra("message", message);
+            startActivity(intent);
+        }
+    }
+
+    public void saveMessageToFirebase(String location){
+        mMessageReference.setValue(location);
     }
 }
+
+
