f8d65274db497798babd9df9ab64cd21e1a5f588
5adiyah <sadiali@pdx.edu>
2016-07-14 11:16:42 -0700

    Display username

--- app/src/main/java/com/example/guest/messageme/MainActivity.java
+++ app/src/main/java/com/example/guest/messageme/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.guest.messageme;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,9 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private DatabaseReference mMessageReference;
     private ValueEventListener mMessageReferenceListener;
     private FirebaseRecyclerAdapter mAdapter;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     View mView;
     Context mContext;
@@ -34,12 +39,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.newMessage) ImageView mNewMessage;
     @Bind(R.id.logout) ImageView mLogout;
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.username) TextView mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    mUserName.setText(user.getDisplayName());
+                } else {
+
+                }
+            }
+        };
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MESSAGE);
 
@@ -68,6 +87,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         } else if(v==mLogout){
             logout();
+        }
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
